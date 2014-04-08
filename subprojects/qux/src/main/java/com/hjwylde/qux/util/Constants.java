0f282a0977d9057fbe5a7f5d04bf91c46ead0ef4
Henry J. Wylde <accounts+git@hjwylde.com>
2014-04-08 18:40:47 +1200

    Preparation for v0.1.1

--- subprojects/qux/src/main/java/com/hjwylde/qux/util/Constants.java
+++ subprojects/qux/src/main/java/com/hjwylde/qux/util/Constants.java
@@ -15,11 +15,15 @@ public final class Constants {
      * Qux v0.1.0.
      */
     public static final int QUX0_1_0 = 0 << 16 | 1 << 8 | 0;
+    /**
+     * Qux v0.1.1.
+     */
+    public static final int QUX0_1_1 = 0 << 16 | 1 << 8 | 1;
 
     /**
      * A list of supported Qux versions for this API.
      */
-    public static final ImmutableList<Integer> SUPPORTED_VERSIONS = ImmutableList.of(QUX0_1_0);
+    public static final ImmutableList<Integer> SUPPORTED_VERSIONS = ImmutableList.of(QUX0_1_1);
 
     /**
      * This class cannot be instantiated.
